53c7a4ef38a6ff3efeb6b573d5477e0a1168885f
Wasder1922 <106603311+Wasder1922@users.noreply.github.com>
2024-06-19 21:28:33 +0300

    Программа запуска

--- run_shadow.py
+++ run_shadow.py
@@ -6,8 +6,10 @@ from shadow.polyedr import Polyedr
 
 
 tk = TkDrawer()
+list_figure = ['figure1', 'figure2', 'figure3', 'figure4', 'figure5',
+      "ccc", "cube", "box", "king", "cow"]
 try:
-    for name in ["ccc", "cube", "box", "king", "cow"]:
+    for name in list_figure:
         print("=============================================================")
         print(f"Начало работы с полиэдром '{name}'")
         start_time = time()
