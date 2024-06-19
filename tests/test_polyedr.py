224fc310244f2283657b18a74369e81e92761b0f
Wasder1922 <106603311+Wasder1922@users.noreply.github.com>
2024-06-19 21:27:35 +0300

    Тесты (полиэдр)

--- tests/test_polyedr.py
+++ tests/test_polyedr.py
@@ -36,3 +36,55 @@ class TestPolyedr(unittest.TestCase):
 
     def test_num_edges(self):
         self.assertEqual(len(self.polyedr.edges), 16)
+
+    # Свои простые фигуры для тестов
+
+    # Две грани, одна полностью помещает в тень другую,
+    # притом у «нижней» два ребра длины 5 подходят
+    def test_figure1(self):
+        polyedr = Polyedr('data/figure1.geom')
+        answer = polyedr.calculate_sum()
+        self.assertEqual(answer, 10.0)
+
+    # Всё те же две грани, но теперь повернуты так,
+    # что одно подходящее по условиям ребро стало видимым
+    # (поэтому длина теперь 5)
+    def test_figure2(self):
+        polyedr = Polyedr('data/figure2.geom')
+        answer = polyedr.calculate_sum()
+        self.assertEqual(answer, 5.0)
+
+    # Всё те же две грани, но перевёрнуты
+    # на pi радиан по оси Oy => все ребра
+    # стали видны и ответ 0.0
+    def test_figure3(self):
+        polyedr = Polyedr('data/figure3.geom')
+        answer = polyedr.calculate_sum()
+        self.assertEqual(answer, 0.0)
+
+    # Для одной грани нет затемненных рёбер,
+    # значит и нет подходящих по условию
+    def test_figure4(self):
+        polyedr = Polyedr('data/figure4.geom')
+        answer = polyedr.calculate_sum()
+        self.assertEqual(answer, 0.0)
+
+    # Вертикальная грань не отбрасывает тень по условию
+    def test_figure5(self):
+        polyedr = Polyedr('data/figure5.geom')
+        answer = polyedr.calculate_sum()
+        self.assertEqual(answer, 0.0)
+
+    # Коэффициент гомотетии не изменяет сам полиэдр
+    # файл идентиен с figure1 за исключением к. гомотетии
+    def test_figure6(self):
+        polyedr = Polyedr('data/figure6.geom')
+        answer = polyedr.calculate_sum()
+        self.assertEqual(answer, 10.0)
+
+    # Коэффициент гомотетии не изменяет сам полиэдр
+    # файл идентичен с figure2 за исключением к. гомотетии
+    def test_figure7(self):
+        polyedr = Polyedr('data/figure7.geom')
+        answer = polyedr.calculate_sum()
+        self.assertEqual(answer, 5.0)
